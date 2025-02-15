88fd6f71e304c09ebabe34c2bdc36367741b8ae1
Your Name <your_email@example.com>
2025-02-15 21:32:42 +0300

    initial commit

--- media/asgi.py
+++ media/asgi.py
@@ -1,16 +1,7 @@
-"""
-ASGI config for media project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'media.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'media.settings.development')
 
 application = get_asgi_application()
